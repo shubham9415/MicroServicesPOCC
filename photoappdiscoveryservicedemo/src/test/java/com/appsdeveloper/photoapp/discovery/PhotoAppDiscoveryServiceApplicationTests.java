ab7c2ede7c9780c8bbd5fdb49fa4195cce89a9d6
shubham9415 <shubham.mahindru9415@gmail.com>
2020-10-30 12:23:43 +0530

    Add files via upload
    Discovery Service

---
+++ photoappdiscoveryservicedemo/src/test/java/com/appsdeveloper/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloper.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
